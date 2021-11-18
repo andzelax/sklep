17e2c88415fff0954f8657f8665612c8d814732a
Andżelika Gawinkowska <an_gaw@spoko.pl>
2021-11-18 16:00:48 +0100

    test

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ include 'navbar.php';
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="main.css">
